64ce624570a9a659952262d908c0d77f0b66a089
caler <g-i@live.cn>
2017-08-23 23:02:41 +0800

    UPDATE: 1.Hello.c

--- 1.Hello.c
+++ 1.Hello.c
@@ -1,6 +1,15 @@
 #inclued <stdio.h> /* 这是头文件 */
+#define PI 3.14 //定义常量
 int main () // 这是主函数
 {
-    prinf("Hello World!\n");
+    int age = 23;
+    char sex = "M";
+    printf("Hello World!\n");
+    // 占位输出
+    printf("I'm a %c, I'm %d years old.\n", age, sex);
+    print("Pi is %f.\n", PI);
+
+    // 强制类型转换
+    printf("%d", (int)1.2);
     return 0;
 }
